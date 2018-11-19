9d39265d66faf4b967784fd45b7a0a3550057e36
suriya <suriyaJaay@users.noreply.github.com>
2018-11-19 11:34:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,42 +14,42 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-app.post('/dropMail', (req, res)=> { 
-  console.log("contents",req.body);  
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
-    auth: {
-      user: 'amsuriya1989@gmail.com',
-      pass: 'surya2k8',
-    },
-  });
-  let message = {
-    from: '"Demo", mailer@nodemailer.com',
-    to: 'hi.jayasurya@gmail.com',
-    subject: 'New Message From: ' + req.body.name,    
-    text:
-      'Name: ' +
-      req.body.name +
-      '\nEmail: ' +
-      req.body.email +
-      '\nPhone: ' +
-      req.body.phone +
-      '\n    ' +
-      req.body.message,
-    html:
-      '<h4> Name: </h4>' +
-      req.body.name +
-      '<h4>Email: </h4>' +
-      req.body.email +
-      '<h4>Phone: </h4>' +
-      req.body.phone +
-      '<p>    ' +
-      req.body.message +
-      '</p>',
-  };
-  transporter.sendMail(message);
+app.post('/dropMail', (req, res) => {
+    console.log("contents", req.body);
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: 'email@email.com',
+            pass: 'password',
+        },
+    });
+    let message = {
+        from: '"Demo", mailer@nodemailer.com',
+        to: 'myotheremailId@email.com',
+        subject: 'New Message From: ' + req.body.name,
+        text:
+            'Name: ' +
+            req.body.name +
+            '\nEmail: ' +
+            req.body.email +
+            '\nPhone: ' +
+            req.body.phone +
+            '\n    ' +
+            req.body.message,
+        html:
+            '<h4> Name: </h4>' +
+            req.body.name +
+            '<h4>Email: </h4>' +
+            req.body.email +
+            '<h4>Phone: </h4>' +
+            req.body.phone +
+            '<p>    ' +
+            req.body.message +
+            '</p>',
+    };
+    transporter.sendMail(message);
 });
 /*app.get('/downloadPDF', (req,res)=>{
   //let file = path.join(__dirname, 'test.pdf');
@@ -60,6 +60,6 @@ app.post('/dropMail', (req, res)=> {
     }
   });
 });*/
-app.listen(port, function(req, res) {
-  console.log('Server is running at port: ', port);
+app.listen(port, function (req, res) {
+    console.log('Server is running at port: ', port);
 });
